7db89e6328e1312e148d86636ea0026252d91a51
richrobber2 <richardrgguno@hotmail.com>
2026-08-07 19:22:47 -0700

    Wire alliance AI WASM exports

--- rust/openfront-wasm/src/lib.rs
+++ rust/openfront-wasm/src/lib.rs
@@ -162,6 +162,7 @@ include!("units.rs");
 include!("structure_render.rs");
 include!("ai.rs");
 include!("ai_tactics.rs");
+include!("ai_alliance.rs");
 include!("map_lifecycle.rs");
 include!("map_mutation.rs");
 include!("query.rs");
