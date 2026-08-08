7e579b3f1ae63df64c065c1a20dbe65a4d3d5f1e
richrobber2 <richardrgguno@hotmail.com>
2026-08-07 20:07:08 -0700

    Wire Rust AI learning WebAssembly exports

--- rust/openfront-wasm/src/lib.rs
+++ rust/openfront-wasm/src/lib.rs
@@ -164,6 +164,7 @@ include!("ai.rs");
 include!("ai_tactics.rs");
 include!("ai_alliance.rs");
 include!("ai_forecast.rs");
+include!("ai_learning.rs");
 include!("map_lifecycle.rs");
 include!("map_mutation.rs");
 include!("query.rs");
